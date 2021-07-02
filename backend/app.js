ad40ba5bbd06af27eb2646ae5aeea09146078073
Roman Tupikov <musinspiration@yandex.ru>
2021-07-02 22:21:49 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -40,12 +40,12 @@ const corsConfig = {
 };
 
 app.use(helmet());
+app.use(requestLogger);
 app.use(limiter);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(requestLogger);
 
 app.use(cors(corsConfig));
 app.options('*', cors(corsConfig));
